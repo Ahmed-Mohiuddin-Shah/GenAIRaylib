ce1d6d307595f09bb8db738a6cbe221598bff91d
Ahmed-Mohiuddin-Shah <mohiuddinlive@gmail.com>
2024-11-26 14:42:04 +0500

    Update project to use src\main.cpp and comment out SetTargetFPS
    Modified GenAIRaylib.vcxproj to compile src\main.cpp instead of GenAIRaylib.cpp. Updated GenAIRaylib.vcxproj.filters to reflect this change. Commented out SetTargetFPS(60) in main.cpp.

--- GenAIRaylib/src/main.cpp
+++ GenAIRaylib/src/main.cpp
@@ -8,7 +8,7 @@ int main()
 
 	InitWindow(screenWidth, screenHeight, "Gen AI");
 
-	SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
+	// SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
 
 	// Main game loop
 	while (!WindowShouldClose())    // Detect window close button or ESC key
